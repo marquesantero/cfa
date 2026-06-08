ac878eb84513c88bbc6c725600ea5b15755bedef
marquesantero <marquesantero@users.noreply.github.com>
2026-06-08 04:02:46 +0000

    deploy: 7a75d4d935e319d47cae167986515fca629fa00b

---
+++ pt-BR/assets/js/acecf23e.2043afd0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([["3220"],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/cfa/pt-BR/blog","blogTitle":"Blog","authorsListPath":"/cfa/pt-BR/blog/authors"}')}}]);
---
+++ pt-BR/assets/js/f494497b.a03ec199.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([["4913"],{9816(e){e.exports=JSON.parse('{"metadata":{"permalink":"/cfa/pt-BR/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
